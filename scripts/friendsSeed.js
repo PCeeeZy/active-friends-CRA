854f48feb96a738dd09a1768166f2852962d3749
Paul Cwik <paul@cwik.us>
2019-04-29 11:06:10 -0500

    create-react-app run and folder structure.  server setup. most of client components and pages as well

---
+++ scripts/friendsSeed.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        name: 'Jordyn',
+        active: true,
+    },
+    {
+        name: 'Mikenzi',
+        active: true,
+    },
+    {
+        name: 'Jake',
+        active: false
+    }
+];
